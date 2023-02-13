8bb925e6fc92e6fdcd6500bab6e022a2305c90e1
AmarrPrime <zloybirik@gmail.com>
2023-02-13 21:53:49 +0300

    suffered out

--- java/homework/lesson1/task2.java
+++ java/homework/lesson1/task2.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Вывести все простые числа от 1 до 1000
 
 
+public class task2 {
+    public static void main(String[] args) {
+        
+        List<Integer> primes = new ArrayList<>();        
+        for (int i = 2; i < 1001; i++) {
+            boolean isPrime = true;
+            for (Integer prime : primes) {
+                if(i%prime == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                primes.add(i);
+            }
+        }
+        System.out.println(primes);
+    }
+    
+}
